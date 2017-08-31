bb904c4833a036e48b5844e6323495b8cbb460cc
AK-PC\Ankris <merkel.naller@gmail.com>
2017-08-31 02:07:53 +0200

    cleanup

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,56 +6,56 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := enableswap.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/enableswap.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := factory_init.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/factory_init.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.mt6592
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/fstab.mt6592
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.modem.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/init.modem.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6592.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/init.mt6592.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6592.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/init.mt6592.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.mt6592.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/init.recovery.mt6592.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.mt6592.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := eng
 LOCAL_SRC_FILES    := boot/ramdisk/ueventd.mt6592.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
